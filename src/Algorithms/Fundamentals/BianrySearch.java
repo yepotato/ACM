5e3db9e31a9695d7e3565f01da1faeccb67dd016
““potato” <1061507109@qq.com>
2016-06-23 15:44:28 +0800

    二分查找优化

--- src/Algorithms/Fundamentals/BianrySearch.java
+++ src/Algorithms/Fundamentals/BianrySearch.java
@@ -1,4 +1,7 @@
 package Algorithms.Fundamentals;
+
+import java.util.Scanner;
+
 /**
  * 二分查找的递归实现
  * @author gzm
@@ -8,7 +11,14 @@ public class BianrySearch {
 
 	public static void main(String[] args) {
 		int a[]={1,2,3,4,5,6,7,8,11,12,13,14,15};
-		int key = 10;
+		int key = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("待查询数组为：");
+		for(int i:a){
+			System.out.print(i+"\t");
+		}
+		System.out.println("\n请输入需要查询的数字：");
+		key = scanner.nextInt();
 		int loc = rank(key,a);
 		if(loc<0){
 			System.out.println("数组中不存在数字："+key);
