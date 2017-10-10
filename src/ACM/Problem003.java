9642b09875f6ba82e55df1696c7868000ec7b3b3
gaozemin <1061507109@qq.com>
2017-10-10 13:47:09 +0800

    字符排序问题

--- src/ACM/Problem003.java
+++ src/ACM/Problem003.java
@@ -1,8 +1,40 @@
 package ACM;
 
+import java.util.Scanner;
+
 public class Problem003 {
 	public static void main(String[] args){
-		//11
+	    Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        String str[]=new String[num];
+        for(int i=0;i<num;i++){
+            str[i] = in.next();
+        }
+        for(int i=0;i<num;i++){
+            char[] ch= str[i].toCharArray();
+            ch = sort(ch);
+            for(int l=0;l<ch.length;l++){
+                System.out.print(ch[l]+" ");
+            }
+            System.out.println();
+        }
+        
 	}
+	private static  char[] sort(char str[]) {
+	    for(int i=0;i<str.length;i++){
+	        for(int j=i;j<str.length;j++){
+	            if(str[i]>str[j]){
+	                switchChar(str,i,j);
+	            }
+	        }
+	    }
+	    return str;
+    }
+    private static void switchChar(char[] str, int i, int j) {
+           char temp;
+           temp =str[i];
+           str[i]=str[j];
+           str[j]=temp;
+    }
 
 }
